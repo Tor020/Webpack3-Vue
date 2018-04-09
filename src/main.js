a27f6b5b22ea8d2ce477a385b57a36f10a63076c
Tor020 <kopp020@gmail.com>
2018-04-09 02:39:33 -0400

    'c'

--- src/main.js
+++ src/main.js
@@ -9,4 +9,16 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#root')
+
+
+// new Vue({
+//   el: '#root',
+//   template: `
+//   <div>
+//   <Rating :grade="3" :maxStars="5" :hasCounter="true"/>
+//   <Rating :grade="4" :maxStars="10"/>
+//   <Rating :grade="0" :maxStars="1" :hasCounter="true"/>
+//   </div>
+// `,  components: { Rating }
+// })
